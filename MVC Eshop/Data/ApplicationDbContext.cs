60e24ac79c21c6e46229ab719c6d666c3f90b0e7
Brejdy <jan.j.bradac@seznam.cz>
2024-07-31 22:30:37 +0200

    Add project files.

---
+++ MVC Eshop/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MVC_Eshop.Models;
+
+namespace MVC_Eshop.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MVC_Eshop.Models.ProductModel> ProductModel { get; set; } = default!;
+    }
+}
---
+++ MVC Eshop/Models/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace MVC_Eshop.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        [DisplayName("Product Name")]
+        public string Name { get; set; } = "";
+        [DisplayName("Product Description")]
+        public string Description { get; set; } = "";
+        [DisplayName("Product Price")]
+        public string Price { get; set; } = "";
+
+    }
+}
